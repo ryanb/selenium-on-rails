aedeaa71ee9fd619bf19e0c6ad5e1d2e4a26a82b
Ryan Bates <ryan@railscasts.com>
2008-06-29 23:05:33 -0700

    look for config file in config/selenium.yml instead of having to place config.yml in plugin dir

--- lib/selenium_on_rails_config.rb
+++ lib/selenium_on_rails_config.rb
@@ -13,10 +13,13 @@ class SeleniumOnRailsConfig
   private
     def self.configs
       unless defined? @@configs
-        file = File.expand_path(File.dirname(__FILE__) + '/../config.yml')
-        @@configs = File.exist?(file) ? YAML.load_file(file) : {}
+        files = [File.expand_path(File.dirname(__FILE__) + '/../config.yml')]
+        files << File.join(RAILS_ROOT, 'config', 'selenium.yml')
+        files.each do |file|
+          @@configs = YAML.load_file(file) if File.exist?(file)
+        end
+        @@configs ||= {}
       end
       @@configs
     end
-
 end
